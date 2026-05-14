e002714a459f33f6417b21d491b6304dcc2efd7d
Kasumi Hanazuki <kasumi@rollingapple.net>
2026-05-14 20:09:02 +0000

    origin isolation

--- src/sw.ts
+++ src/sw.ts
@@ -1,14 +1,31 @@
 /// <reference lib="webworker" />
-declare const self: ServiceWorkerGlobalScope;
-
+import type { WorkboxPlugin } from "workbox-core";
 import { clientsClaim } from "workbox-core";
 import { PrecacheController, PrecacheRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 
+declare const self: ServiceWorkerGlobalScope;
+
 self.skipWaiting();
 clientsClaim();
 
-const precacheController = new PrecacheController({});
+const isolationPlugin: WorkboxPlugin = {
+  handlerWillRespond: async ({ request, response }) => {
+    const headers = new Headers(response.headers);
+    if (request.mode === "navigate") {
+      headers.set("Cross-Origin-Opener-Policy", "same-origin");
+      headers.set("Cross-Origin-Embedder-Policy", "require-corp");
+    }
+    headers.set("Cross-Origin-Resource-Policy", "same-origin");
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers,
+    });
+  },
+};
+
+const precacheController = new PrecacheController({ plugins: [isolationPlugin] });
 precacheController.precache(self.__WB_MANIFEST);
 const precacheRoute = new PrecacheRoute(precacheController);
 registerRoute(precacheRoute);
